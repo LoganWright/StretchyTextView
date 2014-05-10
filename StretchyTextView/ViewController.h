9f6674d215a20c63f15ec3a9fc4e29231f3842eb
Logan Wright <logan.william.wright@gmail.com>
2014-05-10 17:55:53 -0400

    Initial Commit

---
+++ StretchyTextView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StretchyTextView
+//
+//  Created by Logan Wright on 5/10/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StretchyTextView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StretchyTextView
+//
+//  Created by Logan Wright on 5/10/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
